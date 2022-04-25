3fc0c3a5df53f85a83a011fffd6404db9adc43e6
Christian Arce Borro <yerildtt@gmail.com>
2022-04-25 20:25:29 +0200

    hechas algunas funciones relacionadas con el constructor

--- includes/User.hpp
+++ includes/User.hpp
@@ -8,26 +8,26 @@
 /* TODO: hace falta el belongs fuera del código de prueba? */
 
 class User {
-	private:
-		std::string _nickName;
-		bool belongs;
-		int _fd;
-		char _mode;
+    private:
+        std::string _nickName;
+        bool belongs;
+        int _fd;
+        char _mode;
 
-	public:
-		User(int, std::string);
-		User(int, char*, size_t);
-		~User();
+    public:
+        User(int, std::string);
+        User(int, char*, size_t);
+        ~User();
 
-		/* Getters & Setters */
-		inline int getFd() { return _fd; }
-		inline std::string getNickName() { return _nickName; }
-		inline void setNewNick(std::string nick) { _nickName = nick; }
-		inline void setMode(char m) { _mode = m; }
-		inline char getMode() { return _mode; }
+        /* Getters & Setters */
+        inline int getFd() { return _fd; }
+        inline std::string getNickName() { return _nickName; }
+        inline void setNewNick(std::string nick) { _nickName = nick; }
+        inline void setMode(char m) { _mode = m; }
+        inline char getMode() { return _mode; }
 
-		inline bool stillThere() { return belongs; }
-		void leave();
+        inline bool stillThere() { return belongs; }
+        void leave();
 };
 
 #endif
