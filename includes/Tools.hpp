01c0f6580027a55e344c3903cdabd404b0524732
Christian Arce Borro <yerildtt@gmail.com>
2022-09-03 19:35:30 +0200

    quitado using de todos los headers, quedaba uno

--- includes/Tools.hpp
+++ includes/Tools.hpp
@@ -4,9 +4,6 @@
 #include <vector>
 #include <string>
 
-using std::string;
-using std::vector;
-
 namespace irc {
 namespace tools {
 
@@ -23,7 +20,7 @@ void ToUpperCase(std::string &str);
 bool isEqual(const std::string &str1, const std::string &str2);
 bool endsWith(std::string const &str, std::string const &suffix);
 bool hasUnknownFlag(const std::string &str);
-bool hasUnknownChannelFlag(const string &mode);
+bool hasUnknownChannelFlag(const std::string &mode);
 bool charIsInString(const std::string &str, const char c);
 bool anyRepeatedChar(std::string &s);
 
