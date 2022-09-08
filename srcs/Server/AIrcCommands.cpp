f4582aff058000589a682aa7ab01ef7d8a52c100
pmedina-42 <patriciameedina@gmail.com>
2022-09-08 21:41:18 +0200

    [PMA] no falla

--- srcs/Server/AIrcCommands.cpp
+++ srcs/Server/AIrcCommands.cpp
@@ -594,7 +594,6 @@ void AIrcCommands::MODE(Command &cmd, int fd) {
  * Command: QUIT
  * Parameters: [<quitMessage>]
  * The server aknowledges this by sending an error client to the server
- * TODO quit manda mensaje a todos los canales en los que este?
  */
 void AIrcCommands::QUIT(Command &cmd, int fd) {
 
@@ -611,14 +610,14 @@ void AIrcCommands::QUIT(Command &cmd, int fd) {
         message = cmd.args[1][0] == ':' ? cmd.args[1].substr(1) : cmd.args[1];
     }
     for (std::map<std::string, unsigned char>::iterator it = user.ch_name_mask_map.begin(); it != user.ch_name_mask_map.end(); it++) {
-        string reply(user.prefix + " " + cmd.Name() + " " + message);
+        string reply(user.prefix + " " + cmd.Name() + " :" + message);
         std::string ch_name = it->first;
         Channel &channel = getChannelFromName(ch_name);
-        LOG(DEBUG) << "encontrado canal :" << channel.name;
         sendMessageToChannel(channel, reply, user.real_nick);
         channel.deleteUser(user);
     }
     removeUser(fd);
+    closeConnection(fd);
 }
 
 /**
