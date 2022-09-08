89eed5e217f39e429d4d6651326da7e7b9dbdd44
pmedina-42 <patriciameedina@gmail.com>
2022-09-08 22:10:52 +0200

    [PMA] respuesta quit arreglada falta mensaje de error

--- srcs/Server/AIrcCommands.cpp
+++ srcs/Server/AIrcCommands.cpp
@@ -610,7 +610,7 @@ void AIrcCommands::QUIT(Command &cmd, int fd) {
         message = cmd.args[1][0] == ':' ? cmd.args[1].substr(1) : cmd.args[1];
     }
     for (std::map<std::string, unsigned char>::iterator it = user.ch_name_mask_map.begin(); it != user.ch_name_mask_map.end(); it++) {
-        string reply(user.prefix + " " + cmd.Name() + ( size == 2 ? " :Quit: ": " :") + message);
+        string reply(":"+user.prefix +" "+cmd.Name()+ ( size == 2 ? " :Quit: ": " :") +message);
         std::string ch_name = it->first;
         Channel &channel = getChannelFromName(ch_name);
         sendMessageToChannel(channel, reply, user.nick);
